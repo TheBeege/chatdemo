c02d6b3e62e8855168a9ceaca44347ab9f2d103c
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2016-08-08 20:30:06 -0700

    added timestamps

--- public/local.js
+++ public/local.js
@@ -9,31 +9,31 @@ sendButton.addEventListener("click", sendMessage);
 
 // if ENTER key was pressed, send message
 window.addEventListener("keypress", function(event){
-	if (event.which === 13) {		
+	if (event.which === 13) {
 		sendMessage();
-	}	
+	}
 });
 
 // when "chat" event received, display message
 socket.on('chat', function(data){
-    console.log('RECEIVED: name: '+ data.name + ', message: ' + data.message);
-	displayNewMessage(data.name, data.message);
+    console.log('RECEIVED: name: '+ data.name + ', message: ' + data.message + ', timestamp: ' + data.timestamp);
+	displayNewMessage(data.name, data.message, data.timestamp);
 });
 
 
 function sendMessage(event) {
 	console.log('SENDING: name: '+ nameInput.value + ', message: ' + messageInput.value);
-	socket.emit('chat', {name: nameInput.value, message: messageInput.value} );  
+	socket.emit('chat', {name: nameInput.value, message: messageInput.value, timestamp: moment().format("HH:mm:ss")} );
 }
 
-function displayNewMessage (username, message) {
+function displayNewMessage (username, message, timestamp) {
   var newMessage = document.createElement('div');
   newMessage.className = 'message';
   var messageTextNode = document.createTextNode(': ' + message);
 
   var newMessageUser = document.createElement('span');
   newMessageUser.className = 'username';
-  newMessageUser.innerText = username;
+  newMessageUser.innerText = username + ' ' + timestamp.toString();
   newMessage.appendChild(newMessageUser);
   newMessage.appendChild(messageTextNode);
 
@@ -41,4 +41,3 @@ function displayNewMessage (username, message) {
 
   window.scrollTo(0,document.body.scrollHeight);
 }
-
